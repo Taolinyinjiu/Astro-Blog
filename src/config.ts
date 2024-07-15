2f261c02079f95a2ab6f856e1af96acd79d77c02
桃林饮酒 <73354330+Taolinyinjiu@users.noreply.github.com>
2024-07-15 19:43:40 +0800

    test
    hello

---
+++ src/config.ts
@@ -0,0 +1,63 @@
+import type { PostHideElements } from '~/content/config';
+
+export const Site = 'https://astro-blog-zozo.pages.dev';
+export const SiteLanguage = 'zh';
+export const SiteTitle = '摆烂的桃的Blog';
+export const SiteDescription = '回不去的何止时间';
+export const FooterDescription = '勇敢的人不是不落泪的人，而是含着泪水继续勇敢奔跑的人';
+export const AdminName = 'Taolinyinjiu';
+export const PageSize = 15;
+
+// socialPlatform => userName
+// check components/Header.astro socialConfig for more info
+export const Socials: Record<string, Record<string, string>> = {
+	mail: { url: 'mailto:Taolinyinjiu@outlook.com' },
+	github: { url: 'https://github.com/Taolinyinjiu' },
+	x: { url: 'https://x.com/userName' },
+	mastodon: { url: 'https://mastodon.social/@userName' },
+	facebook: { url: 'https://facebook.com/userName' },
+	instagram: { url: 'https://instagram.com/userName' },
+	telegram: { url: 'https://t.me/@userName' },
+	youtube: { url: 'https://youtube.com/@userName' },
+	skype: { url: 'https://skype.com/userName' },
+	slack: { url: 'https://slack.com/userName' },
+	messenger: { url: 'https://messenger.com/userName' },
+	whatsapp: { url: 'https://whatsapp.com/userName' },
+	snapchat: { url: 'https://snapchat.com/userName' },
+	line: { url: 'https://line.com/userName' },
+	twitch: { url: 'https://twitch.com/userName' },
+	weibo: { url: 'https://weibo.com/userName' },
+	bilibili: { url: 'https://space.bilibili.com/342976194' },
+	dingding: { url: 'https://dingtalk.com/userName' },
+	zhihu: { url: 'https://zhihu.com/userName' },
+	douban: { url: 'https://douban.com/userName' },
+	rss: { url: '/rss.xml' },
+};
+
+// doc: https://giscus.app
+// data-theme is auto changed between noborder_light / noborder_gray
+export const GiscusConfig: Record<string, string> = {
+	'data-repo': 'ladit/astro-blog-zozo',
+	'data-repo-id': 'R_kgDOLgobXQ',
+	'data-category': 'Announcements',
+	'data-category-id': 'DIC_kwDOLgobXc4Cd_N6',
+	'data-mapping': 'pathname',
+	'data-strict': '0',
+	'data-reactions-enabled': '1',
+	'data-emit-metadata': '0',
+	'data-input-position': 'top',
+	'data-lang': 'zh-CN',
+	'data-loading': 'lazy',
+	crossorigin: 'anonymous',
+	async: '',
+};
+
+export type HideElements =
+	| PostHideElements
+	| 'logo'
+	| 'search'
+	| 'themeToggler'
+	| 'siteDescription'
+	| 'footerDescription';
+// Always hide elements from site
+export const Hide: HideElements[] = [];
